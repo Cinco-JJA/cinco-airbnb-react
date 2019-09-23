a02392a5769cd0d2fa9b5d183ac754a01ad35b5a
Andres Weber <andresmweber@gmail.com>
2019-09-23 19:48:47 -0400

    Tailored seeds.js to the JSON information presented to us in data.json.  Correctly outputted a dataCorrected.json with 11.3k entries.  Created seedDB which can seed the local mongoDB with info from dataCorrected.json.

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,85 +1,120 @@
-const csv = require('csvtojson')
 const fs = require('fs')
-const Hazard = require('../models/Listing')
+const Listing = require('../models/Listing')
 const mongoose = require('mongoose')
 
-
-function connectToDB() {
-    const uri = process.env.MONGODB_URI || `mongodb://localhost/cinco`
+/**
+ * This function will connect to the database and return the connection object.  
+ * If deploying to prod must have process.env.MONGODB_URI defined!
+ * @param {String} dbName The database name you're looking to connect to (defaults to "cinco")
+ * @returns {mongoose.Types.Connection} the connection object to the database you are connected to.
+ */
+function connectToDB(dbName = "cinco") {
+    const uri = process.env.MONGODB_URI || `mongodb://localhost/${dbName}`
     mongoose
-        .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex = true })
+        .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(db => console.log(`Connected to Mongo! Database name: "${db.connections[0].name}"`))
         .catch(err => console.error('Error connecting to mongo', err))
-
+    mongoose.useCreateIndex = true
     var db = mongoose.connection;
-
-    connection.on('connected', () => console.log('Connected to MongoDB database'));
-    connection.on('disconnected', () => console.log('Disconnected from MongoDB database'));
-    connection.on('SIGINT', () => {
-        connection.close(() => {
+    db.on('connected', () => console.log('Connected to MongoDB database'));
+    db.on('disconnected', () => console.log('Disconnected from MongoDB database'));
+    db.on('SIGINT', () => {
+        db.close(() => {
             console.log('Lost connection to MongoDB database due to process termination');
             process.exit();
         });
     });
     db.collection('cinco').conn.collections.listings.createIndex({ location: "2dsphere" })
-    return connection
+    return db
 }
 
 /**
- * This is meant to be run on data.csv coming in from Talal.  It only converts to JSON and hardcodes the filepath.
+ * This is meant to be run on a data set that you need to shift keys for and that has been written in format:
+ * 
+ * Object(
+ *      Key
+ *          entryIndexIdentifier
+ *              value
+ * )
+ * 
+ * that should be in format:
+ * 
+ * Array(
+ *      Object(
+ *          key: value         
+ *      )
+ * )
+ * 
+ * @param {String} file Relative (to the bin directory) or absolute file path to use as input JSON data. Default: './data.json'
+ * @param {Object} corrections Input object with new key mappings in format {key: desiredKey}.  Default: can be seen in the function body.
+ * @example writeCorrectedJSONFile(undefined, {cleaning_fee: "cleaning"})"
+ * @example writeCorrectedJSONFile("otherFile.json", {cleaning_fee: "cleaning", "property_type": "property"})"
  */
-function convertCSVtoJSON() {
-    csv()
-        .fromFile('data.csv')
-        .then((crimeDataSet) => {
-            fs.writeFile("data.json", JSON.stringify(crimeDataSet), 'utf8', function(err) {
-                if (err) {
-                    console.log("An error occurred while writing JSON Object to File.");
-                    return console.log(err);
-                }
-                console.log("JSON file has been saved.");
-            });
-        })
-}
-
-/**
- * This is meant to be run on dataGeocoded (After running geolocation.py).  
- * It renames all fields to the proper field names in Hazard.
- */
-function fillCollectionFromJSON() {
-    connectToDB()
-    let rawData = fs.readFileSync('./dataGeocoded.json');
-    let crimeDataSet = JSON.parse(rawData);
-
-    let crimeDataSetCorrected = crimeDataSet.map((crimeData, i) => {
-        const { Description, Location, Agency, location } = crimeData
-        const incidentNum = crimeData['Incident#']
-        const crimeDate = crimeData['Date']
-        return {
-            description: Description,
-            address: Location,
-            agency: Agency,
-            date: crimeDate,
-            location: {
-                type: "Point",
-                coordinates: location || [0, 0]
-            },
-            incidentNum
+async function writeCorrectedJSONFile(file, corrections) {
+    if (!corrections) {
+        corrections = {
+            minimum_nights: "minNights",
+            security_deposit: "securityDeposit",
+            cleaning_fee: "cleaningFee",
+            review_scores_rating: "reviewRating",
+            number_of_reviews: "numReviews"
         }
-    })
+    }
+    if (!file) file = './data.json'
 
-    Hazard.collection.insertMany(crimeDataSetCorrected, function(err, results) {
+    let rawData = fs.readFileSync(file);
+    let dataSet = JSON.parse(rawData);
+    console.log(`Loaded data set from file ${file}`)
+
+    let dataKeys = Object.keys(dataSet);
+    let dataArrays = dataKeys.map(key => Object.keys(dataSet[key]).map(listingIndex => dataSet[key][listingIndex]))
+    let correctedDataSet = []
+    console.log(`Initial data set main keys: \n ${Object.keys(dataSet).join('\n    ')}`)
+
+    for (let i = 0; i < Object.keys(dataSet[dataKeys[0]]).length; i++) {
+        let dataEntry = dataKeys.map((dataKey, keyIndex) => [(dataKey in corrections) ? corrections[dataKey] : dataKey, dataArrays[keyIndex][i]])
+        correctedDataSet.push(Object.fromEntries(dataEntry.filter(e => e)))
+    }
+
+    console.log(`Corrected data set main keys: ${correctedDataSet}`)
+    console.log(correctedDataSet)
+
+    const outputFile = "dataCorrected.json"
+    fs.writeFile(outputFile, JSON.stringify(correctedDataSet), 'utf8', function(err) {
         if (err) {
-            return console.error(err);
+            console.log("An error occurred while writing JSON Object to File.");
+            return console.log(err);
+        }
+        console.log(`JSON file has been saved to ${outputFile}.`);
+    });
+
+}
+
+
+/**
+ * Seeds the DB with Listing entries from the given input JSON file.
+ * 
+ * @param {String} file Relative (to the bin directory) or absolute file path to use as input JSON data.
+ * @example seedDB()
+ * @example seedDB("otherFile.json")
+ */
+async function seedDB(file = './dataCorrected.json') {
+    let rawData = fs.readFileSync(file);
+    let dataSet = JSON.parse(rawData);
+    console.log(`Loaded data set from file ${file}`)
+    await connectToDB()
+    await Listing.collection.insertMany(dataSet, async function(err, results) {
+        if (err) {
+            console.error(err);
         } else {
             console.log("Multiple documents inserted to Collection...closing...");
-            mongoose.connection.close()
+            await mongoose.connection.close()
         }
     });
 }
 
 module.exports = {
-    fillCollectionFromJSON,
-    convertCSVtoJSON,
-    connectToDB
+    writeCorrectedJSONFile,
+    connectToDB,
+    seedDB
 }
